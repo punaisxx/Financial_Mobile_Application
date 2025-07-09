90aafa1352ddb33940d9d61b9ab2213bccff2126
punaisxx <rawin.nx@gmail.com>
2025-07-09 11:40:32 +0700

    add AppRoutes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String login = '/login';
+  static const String swap = '/swap';
+  static const String confirmation = '/confirmation';
+  static const String success = '/success';
+}
